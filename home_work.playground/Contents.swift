0999edcc3be5a4804b995536606245c314814295
Igor Yarovoy <iyarovoy777@gmail.com>
2023-03-14 17:50:09 +0300

    second_commit

--- home_work.playground/Contents.swift
+++ home_work.playground/Contents.swift
@@ -37,3 +37,76 @@ telek.showCurrentChannel()
 
 telek.isOn = false
 telek.showCurrentChannel()
+
+
+struct SettingsTV  {
+    var volume: Double
+    var isColorView: Bool
+}
+
+enum VideoFormat: String {
+    case tvChannel   = "режим просмотра каналов"
+    case videoByPort = "режим видео по входящему видео порту"
+}
+class NewTv: Tv {
+    private var currentVolume: Double = 0
+    var currentVideoFormat: VideoFormat
+    var maxVolume: Double = 1
+    var settings: SettingsTV {
+        willSet {
+            if newValue.volume > maxVolume {
+                self.currentVolume = 1
+            } else if newValue.volume < 0 {
+                self.currentVolume = 0
+            } else {
+                currentVolume = newValue.volume
+            }
+        }
+    }
+
+    init(currentVideoFormat: VideoFormat, settings: SettingsTV) {
+        self.currentVideoFormat = currentVideoFormat
+        self.settings = settings
+        super.init(model:("Заря"), isOn: true, currentChannel: Channels.firstChannel)
+    }
+    
+    override func showCurrentChannel(){
+        var colorViewDesc: String
+        if settings.isColorView {
+            colorViewDesc = "цветной"
+        } else {
+            colorViewDesc = "черно-белый"
+        }
+        
+        if currentVideoFormat == .tvChannel {
+            print("Вы смотрите \(currentVideoFormat.rawValue).")
+            print("Активный канал: \(currentChannel.rawValue).")
+            print("Режим дисплея: \(colorViewDesc).")
+            print("Громкость: \(currentVolume). ")
+        } else if currentVideoFormat == .videoByPort{
+            print("Вы смотрите \(VideoFormat.videoByPort.rawValue).")
+            print("Режим дисплея: \(colorViewDesc).")
+            print("Громкость: \(currentVolume). ")
+        }
+    }
+}
+
+//Просмотр
+var newTelevision = NewTv(currentVideoFormat: .tvChannel, settings: SettingsTV.init(volume: 0.4, isColorView: true))
+newTelevision.settings.volume = 1.3
+
+newTelevision.showCurrentChannel()
+newTelevision.currentVideoFormat
+
+
+
+//Видео порт
+var newTelevision2 = NewTv(currentVideoFormat :.videoByPort, settings: SettingsTV.init(volume: 0.4, isColorView: true))
+newTelevision2.settings.volume = 0.5
+newTelevision2.showCurrentChannel()
+
+newTelevision2.settings.volume = -1
+newTelevision2.showCurrentChannel()
+
+newTelevision2.settings.volume = 8
+newTelevision2.showCurrentChannel()
