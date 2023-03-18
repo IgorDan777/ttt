9e0bbb6bbbc0587b9c5007d70918acdb9abafd4e
Igor Yarovoy <iyarovoy777@gmail.com>
2023-03-18 22:34:37 +0300

    next

--- Свойства и методы объекта муз.треки.playground/Contents.swift
+++ Свойства и методы объекта муз.треки.playground/Contents.swift
@@ -12,9 +12,9 @@ class Track {
     var name: String
     var compositor: String
     var long: Float
-    var country: String
+    var country: Countries
     
-    init(name: String, compositor: String, long: Float, country: String) {
+    init(name: String, compositor: String, long: Float, country: Countries) {
         self.name = name
         self.compositor = compositor
         self.long = long
@@ -38,10 +38,15 @@ let category = Category()
 
 
 let track = Track(name: "Трава у дома", compositor: "Владимир Мигуля", long: 4.42,
-                  country: Countries.USSR.rawValue)
+                  country: Countries.USSR)
 
 let secondTrack = Track(name: "Лесник", compositor: "Король и Шут", long: 3.11,
-                        country: Countries.Russia.rawValue)
+                        country: Countries.Russia)
+
+let thirdChannel = Track(name: "Personal Jesus", compositor: "Marilyn Manson", long: 4.06, country: Countries.USA)
+
+track.country.rawValue
+secondTrack.country.rawValue
 
 category.addNewTrack(track: track)
 category.addNewTrack(track: secondTrack)
